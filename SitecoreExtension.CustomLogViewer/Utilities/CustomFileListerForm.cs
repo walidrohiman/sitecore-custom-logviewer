dd860daa88310da7d5caaf2d9cd0dab754fe2144
Walid Rohiman <rohiman.walid@gmail.com>
2021-12-04 22:38:54 +0400

    Initial commit

---
+++ SitecoreExtension.CustomLogViewer/Utilities/CustomFileListerForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Exceptions;
+using Sitecore.IO;
+using Sitecore.Security.Accounts;
+using Sitecore.Shell.Web;
+using Sitecore.Web;
+using Sitecore.Web.UI.HtmlControls;
+using Sitecore.Web.UI.Pages;
+using Sitecore.Web.UI.Sheer;
+using Sitecore.Web.UI.XmlControls;
+
+namespace SitecoreExtension.CustomLogViewer.Utilities
+{
+    public class CustomFileListerForm : DialogForm
+    {
+        protected XmlControl Dialog;
+        protected Listview FileLister;
+
+        protected virtual void CheckSecurity()
+        {
+            ShellPage.IsLoggedIn();
+            User user = Context.User;
+            if (user.IsAdministrator)
+                return;
+            bool flag1 = user.IsInRole("sitecore\\Sitecore Client Developing");
+            bool flag2 = user.IsInRole("sitecore\\Sitecore Client Maintaining");
+            if (!flag1 && !flag2)
+                throw new AccessDeniedException("Application access denied.");
+        }
+
+        protected void DoOK()
+        {
+            ListviewItem[] selectedItems = this.FileLister.SelectedItems;
+            if (selectedItems.Length == 0)
+                SheerResponse.Alert("Select a file.");
+            else if (selectedItems.Length > 1)
+            {
+                SheerResponse.Alert("Select a single file.");
+            }
+            else
+            {
+                SheerResponse.SetDialogValue(selectedItems[0].ServerProperties["Path"] as string);
+                SheerResponse.CloseWindow();
+            }
+        }
+
+        protected void OnFileListerDblClick()
+        {
+            this.DoOK();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Assert.ArgumentNotNull((object)e, nameof(e));
+            this.CheckSecurity();
+            base.OnLoad(e);
+            if (Context.ClientPage.IsEvent)
+                return;
+            UrlHandle urlHandle = UrlHandle.Get();
+            string str1 = urlHandle["ic"];
+            if (!string.IsNullOrEmpty(str1))
+                this.Dialog["Icon"] = (object)str1;
+            string str2 = WebUtil.SafeEncode(urlHandle["he"]);
+            if (str2.Length > 0)
+                this.Dialog["Header"] = (object)str2;
+            string str3 = WebUtil.SafeEncode(urlHandle["txt"]);
+            if (str3.Length > 0)
+                this.Dialog["Text"] = (object)str3;
+            string str4 = WebUtil.SafeEncode(urlHandle["btn"]);
+            if (str4.Length > 0)
+                this.Dialog["OKButton"] = (object)str4;
+            string path1 = urlHandle["fo"];
+            string searchPattern = urlHandle["flt"];
+            string path2 = FileUtil.MapPath(path1);
+            if (urlHandle["di"] == "1")
+            {
+                foreach (string directory in Directory.GetDirectories(path2))
+                {
+                    ListviewItem listviewItem = new ListviewItem();
+                    this.FileLister.Controls.Add((System.Web.UI.Control)listviewItem);
+                    listviewItem.ID = Sitecore.Web.UI.HtmlControls.Control.GetUniqueID("I");
+                    listviewItem.Header = Path.GetFileName(directory);
+                    listviewItem.Icon = "Applications/16x16/folder.png";
+                    listviewItem.ServerProperties["Path"] = (object)FileUtil.UnmapPath(directory);
+                }
+            }
+
+            string[] subFolders = Directory.GetDirectories(path2);
+            foreach (var subFolder in subFolders)
+            {
+                ListviewItem listviewItem = new ListviewItem();
+                this.FileLister.Controls.Add((System.Web.UI.Control)listviewItem);
+                listviewItem.ID = Sitecore.Web.UI.HtmlControls.Control.GetUniqueID("I");
+                listviewItem.Header = Path.GetFileName(subFolder);
+                listviewItem.Icon = "Applications/16x16/folder.png";
+                listviewItem.ServerProperties["Path"] = (object)FileUtil.UnmapPath(subFolder);
+            }
+
+            foreach (string file in Directory.GetFiles(path2, searchPattern))
+            {
+                ListviewItem listviewItem = new ListviewItem();
+                this.FileLister.Controls.Add(listviewItem);
+                listviewItem.ID = Control.GetUniqueID("I");
+                listviewItem.Header = Path.GetFileName(file);
+                listviewItem.Icon = "Applications/16x16/document.png";
+                listviewItem.ServerProperties["Path"] = (object)FileUtil.UnmapPath(file);
+                FileInfo fileInfo = new FileInfo(FileUtil.MapPath(file));
+                listviewItem.ColumnValues["size"] = (object)MainUtil.FormatSize(fileInfo.Length);
+                listviewItem.ColumnValues["modified"] = (object)DateUtil.FormatShortDateTime(DateUtil.ToServerTime(fileInfo.LastWriteTimeUtc));
+            }
+        }
+
+        protected override void OnOK(object sender, EventArgs args)
+        {
+            Assert.ArgumentNotNull(sender, nameof(sender));
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            this.DoOK();
+        }
+    }
+}
---
+++ SitecoreExtension.CustomLogViewer/Utilities/CustomLogViewerForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using Sitecore;
+using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.IO;
+using Sitecore.Shell.Applications.Reports.LogViewer;
+using Sitecore.Text;
+using Sitecore.Web;
+using Sitecore.Web.UI.Sheer;
+
+namespace SitecoreExtension.CustomLogViewer.Utilities
+{
+    public class CustomLogViewerForm : LogViewerForm
+    {
+        [HandleMessage("logviewer:Open", true)]
+        public new void Open(ClientPipelineArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            if (args.IsPostBack)
+            {
+                if (args.Result.Length <= 0 || args.Result == "undefined")
+                    return;
+                string file = GetFile(args.Result);
+                if (string.IsNullOrEmpty(file))
+                    SheerResponse.Alert("You can only open log files.");
+                else if (!file.Contains(".txt")) //open sub folders found in logs
+                {
+                    ListFiles("Open Log File", " ", "Software/32x32/text_code_colored.png", nameof(Open), file, "*.txt", false);
+                    args.WaitForPostBack();
+                }
+                else
+                    this.SetFile(file);
+            }
+            else
+            {
+                ListFiles("Open Log File", " ", "Software/32x32/text_code_colored.png", nameof(Open), Settings.LogFolder, "*.txt", false);
+                args.WaitForPostBack();
+            }
+        }
+        private static string GetFile(string file)
+        {
+            if (string.IsNullOrEmpty(file) || !FileUtil.MapPath(file).StartsWith(FileUtil.MapPath(Settings.LogFolder), StringComparison.InvariantCultureIgnoreCase))
+                return string.Empty;
+            return file;
+        }
+
+        private void SetFile(string filename)
+        {
+            if (string.IsNullOrEmpty(filename))
+            {
+                Context.ClientPage.ServerProperties["File"] = (object)null;
+                this.Document.SetSource("control:LogViewerDetails", string.Empty);
+                Context.ClientPage.ClientResponse.SetInnerHtml("Commandbar_CommandbarTitle", Translate.Text("Log Files"));
+                Context.ClientPage.ClientResponse.SetInnerHtml("Commandbar_CommandbarDescription", Translate.Text("This tool displays the content of log files."));
+                this.HasFile.Disabled = true;
+            }
+            else
+            {
+                Context.ClientPage.ServerProperties["File"] = (object)filename;
+                this.Document.SetSource("control:LogViewerDetails", "file=" + HttpUtility.UrlEncode(filename));
+                FileInfo fileInfo = new FileInfo(FileUtil.MapPath(filename));
+                Context.ClientPage.ClientResponse.SetInnerHtml("Commandbar_CommandbarTitle", Path.GetFileNameWithoutExtension(filename));
+                Context.ClientPage.ClientResponse.SetInnerHtml("Commandbar_CommandbarDescription", Translate.Text(Translate.Text("Last access: {0}") + "<br/>", (object)DateUtil.FormatShortDateTime(DateUtil.ToServerTime(fileInfo.LastWriteTimeUtc))) + Translate.Text("Size: {0}", (object)MainUtil.FormatSize(fileInfo.Length)));
+                this.HasFile.Disabled = false;
+            }
+        }
+
+        public static void ListFiles(string header, string text, string icon, string button, string folder, string filter, bool directories)
+        {
+            Assert.ArgumentNotNull((object)header, nameof(header));
+            Assert.ArgumentNotNull((object)text, nameof(text));
+            Assert.ArgumentNotNull((object)icon, nameof(icon));
+            Assert.ArgumentNotNull((object)button, nameof(button));
+            Assert.ArgumentNotNull((object)folder, nameof(folder));
+            Assert.ArgumentNotNull((object)filter, nameof(filter));
+            Assert.ArgumentNotNull((object)directories, nameof(directories));
+
+            UrlString urlString = new UrlString(UIUtil.GetUri("control:CustomFileLister"));
+            new UrlHandle()
+            {
+                ["he"] = header,
+                ["txt"] = text,
+                ["ic"] = icon,
+                ["btn"] = button,
+                ["fo"] = folder,
+                ["flt"] = filter,
+                ["di"] = (directories ? "1" : "0")
+            }.Add(urlString);
+            Context.ClientPage.ClientResponse.ShowModalDialog(new ModalDialogOptions(urlString.ToString())
+            {
+                Response = true,
+                MinWidth = "580px"
+            });
+        }
+    }
+}
